3de8607d536ea6e0edee928ada58dbc225605e8e
Melo Mashabane <melomashabane@192.168.1.101>
2021-01-12 08:19:54 +0000

    Add Counter component

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Counter from './components/counter';
 
 const element = <h1>Helloooooow Moshy</h1>
 console.log(element);
 
-ReactDOM.render(element, document.getElementById("root"));
+ReactDOM.render(<Counter />, document.getElementById("root")); 
