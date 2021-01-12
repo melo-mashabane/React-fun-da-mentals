0fc5effc74fc9dea46440331e60ae14433cd7a5d
Melo Mashabane <melomashabane@192.168.1.101>
2021-01-12 10:37:48 +0000

    Add embedded image expression

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,12 +3,14 @@ import React, { Component } from 'react';
 class Counter
  extends Component {
     state = {
-        count: 0
+        count: 0,
+        imageUrl: "https://iwalkpaths.co.uk/wordpress/wp-content/uploads/2019/10/iwalk_logo.jpeg"
     }
 
     render() { 
         return (
         <React.Fragment>
+            <img src={this.state.imageUrl} alt="" />
             <span>{this.formatCount()}</span>
             <button>Increment</button>
         </React.Fragment>
