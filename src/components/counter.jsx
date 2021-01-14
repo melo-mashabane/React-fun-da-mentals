28f632aace4b76759a5eef36bcfaba91527bee05
Melo Mashabane <melomashabane@192.168.1.101>
2021-01-14 15:22:14 +0000

    Add render method

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -7,16 +7,20 @@ class Counter
         imageUrl: "https://iwalkpaths.co.uk/wordpress/wp-content/uploads/2019/10/iwalk_logo.jpeg"
     }
 
-
-
     render() { 
         return (
         <React.Fragment>
             <img src={this.state.imageUrl} alt="" />
-            <span style={{fontSize: 50}} className="badge badge-primary m-2">{this.formatCount()}</span>
+            <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
             <button className="btn btn-secondary btn-sm">Increment</button>
         </React.Fragment>
       );
+    }
+
+    getBadgeClasses() {
+        let classes = "badge m-2 badge-";
+        classes += this.state.count === 0 ? "warning" : "primary";
+        return classes;
     }
 
     formatCount(){
