660f1bd7acfb801692d18f233614e322a1401d3d
Melo Mashabane <melomashabane@192.168.1.101>
2021-01-15 15:51:33 +0000

    Update state

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,12 +3,6 @@ import React, { Component } from 'react';
 class Counter
  extends Component {
 
-    constructor() {
-        super();
-        this.handleIncrement = this.handleIncrement.bind(this);
-    }
-
-
     state = {
         count: 0,
         imageUrl: "https://iwalkpaths.co.uk/wordpress/wp-content/uploads/2019/10/iwalk_logo.jpeg",
@@ -33,16 +27,11 @@ class Counter
         </React.Fragment>
       );
     }
-
-    handleIncrement() {
-        console.log("Increment Clicked!");
-    }
-
     // Experimental: Arrow function inherint the bind word instead of
     // using constructor and rebind every event handler manually.
-    // handleIncrement = () => {
-    //     console.log("Increment Clicked!");
-    // };
+    handleIncrement = () => {
+        this.setState({count : this.state.count + 1});
+    };
 
     renderTags() {
         if(this.state.tags.length === 0) return <p>There are no tags!</p>;
