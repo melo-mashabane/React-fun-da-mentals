60968c0e6e65eb8996a144baeec23ec158716eb9
Melo Mashabane <melomashabane@192.168.1.101>
2021-01-20 15:44:12 +0000

    Remove console log

--- src/index.js
+++ src/index.js
@@ -3,7 +3,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Counters from './components/counters';
 
-const element = <h1>Helloooooow Moshy</h1>
-console.log(element);
-
 ReactDOM.render(<Counters />, document.getElementById("root")); 
