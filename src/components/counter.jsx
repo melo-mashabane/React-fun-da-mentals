8ef78e9de3561fd35b93416530b947c693afa35f
melomashabane <mmashabane190291@gmail.com>
2021-02-04 12:47:38 +0000

    Remove local state

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,16 +1,13 @@
 import React, { Component } from 'react';
 
 class Counter
- extends Component {
-  
-    state = { value: this.props.counter.value};
-    
+ extends Component {    
     render() { 
         return (
         <div>
             <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
             <button 
-                onClick={ () => this.handleIncrement({id: 1})}
+                onClick={() => this.props.onIncrement(this.props.counter)}
                 className="btn btn-secondary btn-sm">Increment
             </button>
             <button 
@@ -20,20 +17,14 @@ class Counter
       );
     }
 
-    // Experimental: Arrow function inherint the bind word instead of
-    // using constructor and rebind every event handler manually.
-    handleIncrement = () => {
-        this.setState({value : this.state.value + 1});
-    };
-
     getBadgeClasses() {
         let classes = "badge m-2 badge-";
-        classes += this.state.value === 0 ? "warning" : "primary";
+        classes += this.props.counter.value === 0 ? "warning" : "primary";
         return classes;
     }
 
     formatCount(){
-        const {value: count} = this.state;
+        const {value: count} = this.props.counter;
         return count === 0 ? "Zero": count;
     }
 }
